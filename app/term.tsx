47b770b8902eb7b827b57c76380a3bd46089b8be
Justin <66831913+stuutzer@users.noreply.github.com>
2024-10-28 22:05:48 +1300

    Added error page for terms not found

--- app/term.tsx
+++ app/term.tsx
@@ -1,11 +1,13 @@
 import { createClient } from '@/lib/supabase/client'
+import { notFound } from 'next/navigation'
 
 export default async function Term({query = "deathtouch"}:{query:string}) {
   console.log(query);
   
   const supabase = createClient();
   const { data: term } = await supabase.from("terms").select().eq('term_name',query).single();
-  const { data: translation } = await supabase.from("translations").select().eq('term_id',term.term_id).single();
+  try{
+    const { data: translation } = await supabase.from("translations").select().eq('term_id',term.term_id).single();
   console.log(translation)
   let card_url = "https://scryfall.com/card/mkc/129/grave-titan"
   let image_url = "https://cards.scryfall.io/png/front/1/3/13680953-cf05-4e38-a3cf-22900c02fab7.png?1706240764"
@@ -29,5 +31,8 @@ export default async function Term({query = "deathtouch"}:{query:string}) {
           </div>
         </div>
     )
+  } catch(error){
+    notFound();
+  }
   }
   
