8eb74246f6941364ae6c25922cbccc8855514fd5
Justin <66831913+stuutzer@users.noreply.github.com>
2024-10-29 17:12:33 +1300

    Added adspace

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,11 @@
 export default function Footer() {
   return (
-    <footer>Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br /> 
-    MTGlossary utilizes card data from <a className="hover-b semi" href="https://scryfall.com/" target="_blank">Scryfall</a>.</footer>
+    <footer>
+      <div className="disclaimer">
+        Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br />
+        MTGlossary utilizes card data from <a className="hover-b semi" href="https://scryfall.com/" target="_blank">Scryfall</a>.
+      </div>
+      <div className="adspace"></div>
+    </footer>
   )
 }
