d6b0f49a24e6fb8e125dce06e94e72ce3a6112dc
Justin <66831913+stuutzer@users.noreply.github.com>
2024-10-29 17:29:52 +1300

    Styling adjustments

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,11 +1,8 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="disclaimer">
-        Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br />
-        MTGlossary utilizes card data from <a className="hover-b semi" href="https://scryfall.com/" target="_blank">Scryfall</a>.
-      </div>
-      <div className="adspace"></div>
+      Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br /> 
+      MTGlossary utilizes card data from <a className="hover-b semi" href="https://scryfall.com/" target="_blank">Scryfall</a>.
     </footer>
   )
 }
