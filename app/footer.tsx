52d64e78a710ca900d554e46c10f868bf5e6f4a1
Justin <66831913+stuutzer@users.noreply.github.com>
2024-10-30 23:22:36 +1300

    Alterations to footer disclaimer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
     <footer>
-      Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br /> 
+      Magic: The Gathering is a Trademark of Wizards of the Coast, Inc. and Hasbro, Inc. MTGlossary is unofficial Fan Content permitted under the <a className="hover-b semi" href="https://company.wizards.com/en/legal/fancontentpolicy" target="_blank">Fan Content Policy</a>.<br />
+      Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.<br/> 
       MTGlossary utilizes card data from <a className="hover-b semi" href="https://scryfall.com/" target="_blank">Scryfall</a>.
     </footer>
   )
